87b2098cc3b2a098d6a6fbe248e6aa03158187cb
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2022-11-23 15:52:06 -0300

    .

---
+++ includes/_footer.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="list1">
+        <ul>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+        </ul>
+    </div>
+    <div class="list2">
+        <ul>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+        </ul>
+    </div>
+    <div class="list3">
+        <ul>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+        </ul>
+    </div>
+    <div class="list4">
+        <ul>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+            <a href="#"><li>Lorem Ipsum is simply</li></a>
+        </ul>
+    </div>
+</footer>
---
+++ includes/_header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Senacflix</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="../assets/js/script.js"></script>
+</head>
+<body>
+    <header>
+        <span class="logo-img"><div class="logo"></div></span>
+        <div class="header-list">
+            <ul>
+                <a href="#"><li>Inicio</li></a>
+                <a href="#"><li>Series</li></a>
+                <a href="#"><li>Minha lista</li></a>
+            </ul>
+        </div>
+        <div class="search-box">
+            <input type="text" name="seach-txt" id="seach-txt">
+            <a href="#">
+                <img src="../img/search.png" alt="">
+            </a>
+        </div>
+    </header>
+</body>
+</html>
