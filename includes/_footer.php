7fb11dcab1a52df89650e1b691b7d40b4b12dc0f
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2022-11-23 19:14:59 -0300

    footer pronto

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,30 +1,34 @@
+<link rel="stylesheet" href="../assets/css/style.css">
+
 <footer>
-    <div class="list1">
-        <ul>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-        </ul>
-    </div>
-    <div class="list2">
-        <ul>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-        </ul>
-    </div>
-    <div class="list3">
-        <ul>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-        </ul>
-    </div>
-    <div class="list4">
-        <ul>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-            <a href="#"><li>Lorem Ipsum is simply</li></a>
-        </ul>
+    <div class="footer-list">
+        <div class="list1">
+            <ul>
+                <a href="#"><li>Central de ajuda</li></a>
+                <a href="#"><li>Serviços</li></a>
+                <a href="#"><li>Termos de uso</li></a>
+            </ul>
+        </div>
+        <div class="list2">
+            <ul>
+                <a href="#"><li>Avisos</li></a>
+                <a href="#"><li>Site</li></a>
+                <a href="#"><li>Quem somos nós</li></a>
+            </ul>
+        </div>
+        <div class="list3">
+            <ul>
+                <a href="#"><li>Privacidade</li></a>
+                <a href="#"><li>Entre em contato</li></a>
+                <a href="#"><li>Cookies</li></a>
+            </ul>
+        </div>
+        <div class="list4">
+            <ul>
+                <a href="#"><li>Instagram</li></a>
+                <a href="#"><li>Twitter</li></a>
+                <a href="#"><li>Youtube</li></a>
+            </ul>
+        </div>
     </div>
 </footer>
