2be03e76706dd9401685e0f3338d30753f449ecc
Enzo Braga <ebdalmoro@gmail.com>
2022-11-29 07:42:00 -0300

    Project style update

--- includes/_header.php
+++ includes/_header.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Senacflix</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <script src="../assets/js/script.js"></script>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script src="./assets/js/script.js"></script>
 </head>
 <body>
     <header>
@@ -27,5 +27,3 @@
             </a>
         </div>
     </header>
-</body>
-</html>
---
+++ main.php
@@ -0,0 +1,13 @@
+<?php 
+include_once './includes/_header.php';
+?>
+<main>
+    <div class="princ_moovie">
+        <h1>Spider-Man</h1>
+        <h2>Coming soon on Senacflix</h2>
+        <button class="trailer_bt">Assita o trailer agora</button>
+    <div>
+</main>
+<?php 
+    include_once './includes/_footer.php';
+?>
