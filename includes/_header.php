6e111de05483f13a18de23cc11698a1ddca9f669
Vinícius Ross Weingaertner dos Santos <viniciusrossws@gmail.com>
2022-11-29 19:53:48 -0300

    main

--- includes/_header.php
+++ includes/_header.php
@@ -7,6 +7,7 @@
     <title>Senacflix</title>
     <link rel="stylesheet" href="./assets/css/style.css">
     <script src="./assets/js/script.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <header>
@@ -23,7 +24,7 @@
         <div class="search-box">
             <input type="text" class="search-txt" placeholder="Procurar">
             <a href="#" class="search-btn">
-                <img src="../img/search.png" alt="">
+                <img src="./img/search.png" alt="">
             </a>
         </div>
     </header>
