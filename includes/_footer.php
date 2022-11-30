aa811ebdabe99630e07a834d5c2f82838edac93e
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2022-11-30 18:17:42 -0300

    links footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -2,30 +2,30 @@
     <div class="footer-list">
         <div class="list1">
             <ul>
-                <a href="#"><li>Central de ajuda</li></a>
-                <a href="#"><li>Serviços</li></a>
-                <a href="#"><li>Termos de uso</li></a>
+                <a href="https://www.senacrs.com.br/faleconosco"><li>Central de ajuda</li></a>
+                <a href="https://www.senacrs.com.br/cursos/livres"><li>Serviços</li></a>
+                <a href="https://senacrs.com.br/hotsite/pdf/codigo-etica-senacrs-OFICIAL.pdf"><li>Termos de uso</li></a>
             </ul>
         </div>
         <div class="list2">
             <ul>
-                <a href="#"><li>Avisos</li></a>
-                <a href="#"><li>Site</li></a>
-                <a href="#"><li>Quem somos nós</li></a>
+                <a href="https://fecomercio-rs.org.br/"><li>Avisos</li></a>
+                <a href="https://www.senacrs.com.br/unidade/84?gclid=EAIaIQobChMI66rv9ejW-wIVuEFIAB1SwQWlEAAYASAAEgKn8PD_BwE"><li>Site</li></a>
+                <a href="https://www.senacrs.com.br/hotsite/ensinomedio/index.php"><li>Quem somos nós</li></a>
             </ul>
         </div>
         <div class="list3">
             <ul>
-                <a href="#"><li>Privacidade</li></a>
-                <a href="#"><li>Entre em contato</li></a>
-                <a href="#"><li>Cookies</li></a>
+                <a href="https://www.senacrs.com.br/hotsite/pdf/pp.pdf"><li>Privacidade</li></a>
+                <a href="https://www.senacrs.com.br/faleconosco"><li>Entre em contato</li></a>
+                <a href="https://www.senacrs.com.br/institucional-premiacoes"><li>Premiações</li></a>
             </ul>
         </div>
         <div class="list4">
             <ul>
-                <a href="#"><li>Instagram</li></a>
-                <a href="#"><li>Twitter</li></a>
-                <a href="#"><li>Youtube</li></a>
+                <a href="https://www.instagram.com/senacrs/"><li>Instagram</li></a>
+                <a href="https://twitter.com/senacrs"><li>Twitter</li></a>
+                <a href="https://www.youtube.com/senacrsoficial"><li>Youtube</li></a>
             </ul>
         </div>
     </div>
