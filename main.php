023dd67f145766f08fdffc90976e39fb184e630c
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2022-11-30 17:32:21 -0300

    mediaplayer

--- main.php
+++ main.php
@@ -5,7 +5,7 @@ include_once './includes/_header.php';
     <div class="princ_moovie">
         <h1 class="Title_start">Spider-Man</h1>
         <h2>Coming soon on Senacflix</h2>
-        <button class="assistir_bt"><i class="fa-solid fa-play"></i>  Assitir</button>
+        <button class="assistir_bt"><i class="fa-solid fa-play"></i>  Assistir</button>
         <button class="assistir_bt"><i class="fa-solid fa-plus"></i>  Minha lista</button>
     </div>
     <div class="catalago">
