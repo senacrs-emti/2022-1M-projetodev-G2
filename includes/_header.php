035172289f4803cf45f40c1b70fc53c8e8f4470a
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2022-11-30 22:21:22 -0300

    main update

--- includes/_header.php
+++ includes/_header.php
@@ -12,12 +12,12 @@
 </head>
 <body>
     <header>
-    <a href="#"><img src="./img/Logo.png" alt="Logo" class="logo_header"></a>
+    <a href="main.php"><img src="./img/Logo.png" alt="Logo" class="logo_header"></a>
         <div class="header-list">
             <nav>
                 <ul>
-                    <a href="#"><li>Inicio</li></a>
-                    <a href="#"><li>Filmes</li></a>
+                    <a href="main.php"><li>Inicio</li></a>
+                    <a href="filmes.php"><li>Filmes</li></a>
                     <a href="#"><li>Minha lista</li></a>
                 </ul>
             </nav>
