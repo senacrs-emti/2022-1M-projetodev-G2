a60d6e8428af8c9253950b7ba31bd0c0e517271d
Enzo Braga <ebdalmoro@gmail.com>
2022-12-01 01:27:44 -0300

    Update cartazes

--- main.php
+++ main.php
@@ -3,47 +3,47 @@ include_once './includes/_header.php';
 ?>
 <main>
     <div class="princ_moovie">
-            <h1 class="Title_start">Homem Aranha</h1>
+            <h1 class="Title_start">Banana</h1>
             <h2>Agora no SenacFlix</h2>
-            <a href="aOrigemDoHomemAranha.php"><button class="assistir_bt"><i class="fa-solid fa-play"></i>  Assistir</button></a>
+            <a href="banana.php"><button class="assistir_bt"><i class="fa-solid fa-play"></i>  Assistir</button></a>
             <button class="assistir_bt"><i class="fa-solid fa-plus"></i>  Minha lista</button>
     </div>
     <div class="catalago">
         <h1 class="title">Produções</h1>
         <div class="carrossel">
             <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image.png" alt="homen aranha" height="400em" width="auto"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image.png" alt="homen aranha" height="400em" width="auto"></a>
             </div>
             <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image (1).png" alt="filme" height="400em" width="auto"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (1).png" alt="filme" height="400em" width="auto"></a>
             </div>
             <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image (2).png" alt="filme" height="400em" width="auto"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (2).png" alt="filme" height="400em" width="auto"></a>
             </div>
             <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image (3).png" alt="filme" height="400em" width="auto"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (3).png" alt="filme" height="400em" width="auto"></a>
             </div>
             <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image (4).png" alt="filme" height="400em" width="auto"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (4).png" alt="filme" height="400em" width="auto"></a>
             </div>
         </div>
     </div>
     <div class="catalago">
         <div class="carrossel">
              <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image (11).png" alt="filme" height="400em" width="auto"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (11).png" alt="filme" height="400em" width="auto"></a>
             </div>
             <div class= "filmes">
-                <a><img src="./img/MicrosoftTeams-image (10).png" alt="filme" height="400em" width="auto"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (10).png" alt="filme" height="400em" width="auto"></a>
             </div>
             <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image (13).png" alt="filme" height="400em" width="auto"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (13).png" alt="filme" height="400em" width="auto"></a>
             </div>
             <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image (14).png" alt="filme" height="400em" width="283em"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (14).png" alt="filme" height="400em" width="283em"></a>
             </div>
             <div class= filmes>
-                <a><img src="./img/MicrosoftTeams-image (8).png" alt="filme" height="400em" width="283em"></a>
+                <a href="#"><img src="./img/MicrosoftTeams-image (8).png" alt="filme" height="400em" width="283em"></a>
             </div>
         </div>
     </div>
