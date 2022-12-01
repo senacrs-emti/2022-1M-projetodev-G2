979d80e9cbb7b876ee7474af7c49d97df5bcfcf6
MauroMFP <mauricioferreiraprederigo@gmail.com>
2022-12-01 16:11:36 -0300

    Filmes

---
+++ filmes.php
@@ -0,0 +1,46 @@
+<?php 
+include_once './includes/_header.php';
+?>
+<main>
+    <div id="catalago">
+        <h1 class="h1">Genero</h1>
+        <div id="carrossel">
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image.png" alt="homen aranha" height="360em" width="240em"></a>
+            </div>
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image (1).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image (2).png" alt="filme" height="360em" width="240m"></a>
+            </div>
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image (3).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image (4).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+        </div>
+        <h1 class="h1">Genero</h1>
+        <div id="carrossel">
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image (11).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+            <div >
+                <a href="#"><img src="./img/MicrosoftTeams-image (10).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image (13).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image (14).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+            <div>
+                <a href="#"><img src="./img/MicrosoftTeams-image (8).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+        </div>
+    </div>
+</main>
+<?php 
+    include_once './includes/_footer.php';
+?>
