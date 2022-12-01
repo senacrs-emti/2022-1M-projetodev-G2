ccac185212119a7bf477d14089278c674b622dfa
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2022-11-30 22:30:03 -0300

    media update

--- clubedos7.php
+++ clubedos7.php
@@ -5,8 +5,8 @@ include_once './includes/_header.php';
     <body>
         <div class="mediaplay">
             <span class="top-vid">
-                <h3>Duração: 00 min</h3>
-                <h1>Titulo</h1>
+                <h3>Duração: 3:43 min</h3>
+                <h1>Banana</h1>
                 <h2>Ano - 2022</h2>
             </span>
             <span class="back-bottom">
@@ -14,8 +14,8 @@ include_once './includes/_header.php';
                 <a href="main.php"><h3>Voltar</h3></a>
             </span>
             <div class="media">
-                <video controls loop width="1000px" height="600px">
-                    <source scr="" type="">
+                <video controls loop width="1400px" height="800px">
+                    <source scr="https://www.youtube.com/watch?v=iw26tjJd4Kk" type="video/mp4">
                 </video>
             </div>
         </div>
