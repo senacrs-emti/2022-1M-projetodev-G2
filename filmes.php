877933e9a3d815ac1f02824b49dc976c6c5bcbe1
MauroMFP <108466315+MauroMFP@users.noreply.github.com>
2022-12-02 09:18:52 -0300

    Filmes

--- filmes.php
+++ filmes.php
@@ -3,28 +3,25 @@ include_once './includes/_header.php';
 ?>
 <main>
     <div id="catalago">
-        <h1 class="h1">Genero</h1>
+        <h1 class="h1">1º Ano</h1>
         <div id="carrossel">
             <div>
-                <a href="aOrigemDoHomemAranha.php"><img src="./img/MicrosoftTeams-image.png" alt="homen aranha" height="360em" width="240em"></a>
-            </div>
-            <div>
-                <a href="distorcao.php"><img src="./img/MicrosoftTeams-image (1).png" alt="filme" height="360em" width="240em"></a>
+                <a href="banana.php"><img src="./img/MicrosoftTeams-image (6).png" alt="filme" height="360em" width="240em"></a>
             </div>
             <div>
                 <a href="again.php"><img src="./img/MicrosoftTeams-image (2).png" alt="filme" height="360em" width="240m"></a>
             </div>
             <div>
-                <a href="paulo.php"><img src="./img/MicrosoftTeams-image (3).png" alt="filme" height="360em" width="240em"></a>
-            </div>
-            <div>
                 <a href="elonmusk.php"><img src="./img/MicrosoftTeams-image (4).png" alt="filme" height="360em" width="240em"></a>
             </div>
-        </div>
-        <h1 class="h1">Genero</h1>
-        <div id="carrossel">
             <div>
-                <a href="incognita.php"><img src="./img/MicrosoftTeams-image (11).png" alt="filme" height="360em" width="240em"></a>
+                <a href="vozes.php"><img src="./img/MicrosoftTeams-image (14).png" alt="filme" height="360em" width="240em"></a>
+            </div>
+        </div>
+        <h1 class="h1">2º Ano</h1>
+        <div id="carrossel">
+        <div>
+                <a href="duasNacoes.php"><img src="./img/MicrosoftTeams-image (8).png" alt="filme" height="360em" width="240em"></a>
             </div>
             <div >
                 <a href="theTrauma.php"><img src="./img/MicrosoftTeams-image (10).png" alt="filme" height="360em" width="240em"></a>
@@ -32,29 +29,17 @@ include_once './includes/_header.php';
             <div>
                 <a href="umaNoiteSigma.php"><img src="./img/MicrosoftTeams-image (13).png" alt="filme" height="360em" width="240em"></a>
             </div>
-            <div>
-                <a href="vozes.php"><img src="./img/MicrosoftTeams-image (14).png" alt="filme" height="360em" width="240em"></a>
-            </div>
-            <div>
-                <a href="duasNacoes.php"><img src="./img/MicrosoftTeams-image (8).png" alt="filme" height="360em" width="240em"></a>
-            </div>
         </div>
-        <h1 class="h1">Genero</h1>
+        <h1 class="h1">3º Ano</h1>
         <div id="carrossel">
             <div>
-                <a href="incognita.php"><img src="./img/MicrosoftTeams-image (11).png" alt="filme" height="360em" width="240em"></a>
-            </div>
-            <div >
-                <a href="theTrauma.php"><img src="./img/MicrosoftTeams-image (10).png" alt="filme" height="360em" width="240em"></a>
+                <a href="aOrigemDoHomemAranha.php"><img src="./img/MicrosoftTeams-image.png" alt="filme" height="360em" width="240em"></a>
             </div>
             <div>
                 <a href="umaNoiteSigma.php"><img src="./img/MicrosoftTeams-image (13).png" alt="filme" height="360em" width="240em"></a>
             </div>
             <div>
-                <a href="vozes.php"><img src="./img/MicrosoftTeams-image (14).png" alt="filme" height="360em" width="240em"></a>
-            </div>
-            <div>
-                <a href="duasNacoes.php"><img src="./img/MicrosoftTeams-image (8).png" alt="filme" height="360em" width="240em"></a>
+                <a href="incognita.php"><img src="./img/MicrosoftTeams-image (11).png" alt="filme" height="360em" width="240em"></a>
             </div>
         </div>
         <h1 class="h1">Recomendados</h1>
