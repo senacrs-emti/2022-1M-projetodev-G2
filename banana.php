a85788807953128223053da4bda103e0318a5df9
Enzux <ebdalmoro@gmail.com>
2022-12-02 10:53:08 -0300

    Banana sinopse update

--- banana.php
+++ banana.php
@@ -11,7 +11,7 @@ include_once './includes/_header.php';
             </span>
             <div class="synopsis">
                 <h2>Sinopse</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at lacinia elit. Donec facilisis nec dolor nec mollis. Aliquam eget tempus tellus. Mauris nec arcu blandit, euismod diam id, pretium tortor. Ut ut urna at nisi dictum semper a at urna. In eleifend mattis est, non elementum elit ornare a. Proin sit amet laoreet mi.</p>
+                <p>Um filme para entusiastas em bananas.</p>
             </div>
             <span class="back-bottom">
                 <a href="main.php"><img src="./img/seta.png" alt=""></a>
